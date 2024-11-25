514c3b55d5e008d5d2e0d3570d19024afc7d0adb
unknown <sciner@ya.ru>
2024-11-25 08:45:38 -0800

    dev

---
+++ blocks.js
@@ -0,0 +1,109 @@
+
+export const textures = {
+    dirt: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAFVBMVEW5hVyHh4eWbEpsbGx0WER5VTpZPSnqDkA3AAAAbklEQVR42hXLwQnDQAwEwOVA7uDyNgq6AtJD/mLD6m0Muv5LCJ7/YES6OhNsvjLGhpXJtA1vW+nMQJy31o+Ncu4uJUwzDSQE13I5giyGGuxqCQTTxnX4RPDkJ+5ChZskgPp6HemIqwp8+ojp8voDZ2EdYwBe7AYAAAAASUVORK5CYII='},
+    bedrock: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAAAAAA6mKC9AAAAcElEQVR42jWPMQ0DQBSFvpazgBgEPFHYbdKmCwth4AZgM0E8ttai9pDjwQNCzZ1tPlMAvEcAwmrjUFF4X3A190e8U/EnkXkrQVeruq8TmUC7LVY8qsE9FVy51TuBCQsAby2fwMPVbcI/An9zNdwD9wFD0lUf95ycGwAAAABJRU5ErkJggg=='},
+    gravel: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAGFBMVEWwrq6xoqKXl5eWjo6JgX6Bf39ya2lkW1tYVrIwAAAAe0lEQVR42g3GwRECIQwF0D/MWIENODtk7YC7DOFulqQANFCAsvXrOz18Uxd3MZBptVUydNZCbf+HueveFO9ncp3CWK+zUCBD/Wjo8VRsHKkNjYjkra6bo41gxMbwHKcY36ExXwaLwVjlkTZBmdIONULmMo4yM/zqyuz+A5MJJSnE6Mt8AAAAAElFTkSuQmCC'},
+    oak_leaves: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAD1BMVEUAAAC5vLmYmZh3dXdoZGjJX6haAAAAAXRSTlMAQObYZgAAAGlJREFUeNoFgAERwyAMAD8pAtJ0AgIYaAEBbI1/TTtSNOaPiyCHDqkwtVvIia7tHzB6p1IatFtW8gazuB93e1j7tBwy8PJtR4+OkW+0cDR2exThSqQeASMalo9QZe2iV8XcmbgRp5W8Rf/WMg1MmmHulQAAAABJRU5ErkJggg=='},
+    oak_log: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAElBMVEWYeEmRcUJ0WjZfSitMPSY4Kxj0FtFUAAAAaklEQVR42hXGQRHDMAwEQI1VBJINwLoTgLgEaiHohD+YOPtaoaXl3BBqas6C5DeVPBkgkOXSkVDQxKpT+8kqX2ZvbrvCYcJ/XHA3iV8sdLslJhI84UnLVvIJH5qxpJXRGSG2Y5D7TXXGxgPEiBE7ybf/RAAAAABJRU5ErkJggg=='},
+    oak_log_top: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAG1BMVEXCnWK4lF+vj1WfhE2WdEF+Yjd0WjZfSitMPSYsJcj4AAAAf0lEQVR42hXJsQ3CMBAF0H/OFxGNfZHoI8gqzMIMjMFCVGwAdQCxgO8EHZKP8NrHY0vhRY3rk8BG62n7aoHpQnzPKtEpxbcAXFksjZ6vQh8+D2StRA0pVjfEQvECFYinqtD0PbX6L2szIGCBBYJLdTsFcCPm4Z5TXbE/SHgp/gPG/zIQFSN2ggAAAABJRU5ErkJggg=='},
+    stone: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAAAAAA6mKC9AAAAZElEQVR42jWNsQ0AMQjEGJcB3HsEr/xSkncBOgkfUyXIguoo4mEXGdkFFvBdlGmpwKCAWIWOeYFladCK4n2BXZZn6vR3dsXRfLnKWfFoXmtUrX81AiboaZ9u4I1G8KdqPICl6AdyLn2NfcJFIAAAAABJRU5ErkJggg=='},
+    stone_bricks: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAFVBMVEWcmZyLiYt/f394dnhqbWpjY2NaWVo/4AZ+AAAAbUlEQVR42hWJsREDIRDEuC3A7F7gmKMDXK5bIHFLXwHwDWBeo0AzAtOD3rhd8qqEnSUOEqZh29cTMZA1wb0iXy+iIeQNhKpqSBn923/9gCbnOhcXSWMh8jLavTaMeR4dCk97lQlZ4ic8IKlUSX8TNSDr6TM/jQAAAABJRU5ErkJggg=='},
+    sand: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAElBMVEXt68vn5Lvj27Daz6PVxJbRuopf1Q47AAAAZklEQVR42gVAwQ3CMBCzQPyRukEcBqjP/dNzWAC6/ywIm0y2GrH72h47eJ1VdqDpOdqF3FuqCK7BzApOv7N+Nlyx126E+nQYbKl1uCYOWcVLWMWMlSDdfnIJ5EtSBdKN5LdADrnIP0wTGY7btIheAAAAAElFTkSuQmCC'},
+    grass_block_top: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAADAFBMVEU3Wx5IeCg2Wh4xUhs1WB0vTho0Vh06YSA3XB8zVRwyUxwyVBw+ZyIpRRc0Vx03XB47YiE4XR84Xh8vTxowUBo+aCI9ZiIzVhw8ZCEuTRkxURs1WR07YiBAaiMsSRhDcCUrSRg1WR4wURs/aiNAayNFciYrSBg/aSMuThosShgwTxo5XyA5YCBDbyUuTBk8ZSFCbiRDcSVHdictShktTBktSxk7YyFGdicAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEdUADQdjt2OxUAAAAAAAABAAAAAABzhKHtiHcAAE8AAAAAADAAAAAAAAAAAABzhPgAAHcAAAAAAAAAAAAAAAB0AAB2OxYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB4gBTQOHcAAIMAfQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGQAAADIAAAAh/UAADYAAAAQAAAANgCDs1jQOAC4AIMAh/V9AAAAAADEAAAAT+10PTau2XcAd3UAAAAAAAAAAAAAAAAAAAAAAADvRADOvYB2LAhP7mgJKQC3diwAAAAAAAAAAACewAB2LAkAAGAABQCAAAAAAAAQAIAAAMBwAAAAAAYAAAAAAwAAAAAAAAB6AHjQOACAAIMAT+4AAAAAAAA4AAAAg9AAAACzYAAAd3cAAAAAAAAAAAAAAAAAAAAAABgAAAAIAAAAT+4AAEAAAABYAAAAT+6o1rAAAAkMAH3zqL15AAAAAWJLR0SL8m9H4AAAAAlwSFlzAAALEwAACxMBAJqcGAAAAOFJREFUGNMNzMligjAUAMDHSwJkaSBhDUgR0ILBKhVr///P6nEuAxAgIZSFUcyFVB8akjA1NrM5K0oMQlFBrV0jZdvCQXXiM+vhSLgZrEM2akMr5aAeu3E6yen8lal5kRqsvZQ+TlaRyPHyNizXssWRs0Z8T4MyFKiIqvZ2b0wnPE1ZAWrt58l3123OkWC0gMl/dGjnTAxY6I16WCNT5r3Gx65GKt9H05HaNCgNhs+NOQ5n47kdyC8fuDuUuwGvjuisD16o1lSqE9Svx3ObAZckEUVU7UBiKe8WUP/diqaV0T9QvxT8vFJhIAAAAABJRU5ErkJggg=='},
+    water_still: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAIABAMAAAB5lPHgAAAAJ1BMVEX////5+fnY2NjW1tbV1dXT09PS0tLPz8/Ozs7CwsK4uLiurq6lpaUTLc7BAAAADXRSTlO0tLS0tLS0tLS0tLS0frXQ3wAABWRJREFUeNqN2D2OHLcSB/AC3pO8/XyCd4B3hHcE5+Ng1qY62UQCBpsosAwoE+AvbDZJD1BbSmXA2MTJGqD/dYOqQ3lYZH/M9IxsBgOCPWz+UF1kk00CN+DwHbkDcHcqP2BxUs8CBsihbm4gVwhEnQSl3Hb0TY9SZ6J0l3KWZ/Io5hR9MkDemqhdiUsAq5YWy/ReKePr/0IAegrP8OPKg5VHyN0E4k4YPd8Wj4bnZoe1ByuPn3k800PxbDoIhPrwPP8rPLb25PBg5VEAh442T3gKz4t0k5D7yWMUoVh4fOVB9dw1Ty6eBDdB/jeZQ+PPBnNIeHDJg/A4BL92tBf04dm9Onq4eczC49c9DjCa53D04MzzI5nntQeTx1cexmHhSdubHsNu8sRYduoxd1t7MITndjd5GPmH2QOYG48ehEeXHl14DsXjloiH4ukemwfNY9DZAzPTUpHqsaVnSNxnoaSeOeJjvPT0xWNLT36qHh095XkNk4dJjsBMQUduHr/msdnzGB7mYeH5YeFRjB6HhcfC05O5ifrSw8i9Ljw8enJj2KkHk2fOnz+b53A/e36ZPRnFcw+SMSH1gkfBZb4LcgqP8EvB/ujBZzxYemT05PBkPvbG0aP5lRQPhuphKZ4vc/XY6DFw8/jCg8mT00UPrniQV55N8zxvhb8gw3bp6U48Xj2+9GRswsPNw8Bh9gyN0Qa1uPPSA1z09EfPo/AN+egZiietPIAUz1P1ZMwehAcCDN1AcuaRhUcNWHu4q567h4gPnlmYyPH6Y3j2PdxeFY/OHm0eyMJze+7Ju+3oOZBe8xjhkifT++LB5Hkhpu602cFt+2KgvnoyqYcHlzwGwO235knV41c9DD3xqDuPnpt3zTMIb2fPvtOj5+eB7hcehEcnD4rnsCN4eLh5+M3Kg7/xWPNg8vxx9DyS5zE+xfP6w0BdeDKTX/EMqXp09ODo4aoYrnmw9txXD33FIC6exEyWO/HJ8+bt2qMrDxPcqqdHtL0k1s95PoVHTz198/xPQCz4/ZCkeXz0vHz7if5zydMSEgCDZfTk0fOmeSQvPK1QVtsK6kbIZk+WM89mJ5mMd6MHxbOlnz5G9g+zB3/reT15GsNOPRuGY/Ro9eCKJ4Wnzi96m5Yeg517ZPJY87gjb2fPetuD4pEzDwQYPQjPvnm4evZp9Ejx/DZ6ZOUZmMQNrj56kJoHuOjBXRo9duLx6lHKOPfE+vzhLdNPOnl89DjQ8plTnV8W45NUD1P+nMc2SVyj5YLn/1ASYH8b+SzNw6OnCw8zuauGx7wtsLNHDf/c47YRXPd81TxblmHhARwfvl96TKMfmfvoEU44JMrhMZAwoJB81RMM24dHTz3SPO+q55Bk6cnFQ++EduGRXPeHQHiiWjxpV9Znnj0MZEH12AUPSE88uc332fO+eNQGBn4tno/NY270AHqqM6XuD9tOODxGCqSE4X72gBlaPKgeu+Bxt9+XHg0PIE5uyHcPNT6PjLwpHjQPLL/sQBIKAVUEEB7U+HDaYehoACQ8zFtBCSbO4qMnnj9mDwuap1cyZL7rABJY5qNt9gBmeBEeg+XwYI4PEJ6cUvEcAIGbEPPt0mPNY2eeZ8C9eMwYzZPj/cV82zyQ8ACwEw8UwJnHJg+HZy+T5656tHqseVTJbfbkyYPJw1/W8852Fx4vnpuFR82x68885sWjkwfMxbOpHqZUPPOyPHqAk/j45MHs+dQVz2HHqXoMyF+QKYDqcUeq5/fxfKqTRyDT+rxdeF7zXb/0+Oq8HELo7GnU/Oboydh3LJNn+OXq95a1R714wNP3DQcT8SYtPHXQtce8emzyWKaH6oEAfwHoySkS2Hjk2AAAAABJRU5ErkJggg=='},
+    short_grass: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAFVBMVEUAAAC4t7igoKCRkZGDgYN2d3ZsbGyS5OzOAAAAAXRSTlMAQObYZgAAAG5JREFUeNptyDESgyAQheHHSvpt7DcDpCaGHMABqRnwBI73P4Ng7Wv+bx6eJgSmAVJguau0cC/eND6AK61dHeS/emCeXJwJEKNe4rhAFirWS4UlU4IsE3xsbTcx4Jda3Fzy0PWs+W8TwnEeOX+2CxhUDj0662krAAAAAElFTkSuQmCC'},
+    dandelion: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAUklEQVQ4y2NgGAXDHfx/4/8fhMnW/O+a5f+Pc1VJNwSkAaZxb5YS2CCyDCFbMwyI17D8B2GyNHr1a/wPXa0Lpsm2HWYACJPlEpABZHsDBIixGQCyM01OrS9sngAAAABJRU5ErkJggg=='},
+    oxeye_daisy: {bin: 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAATlBMVEXr6+tR2/1vtE0nrPtXpDJVnzFRoStQmShVkzJRkTFBixxDhiPBQCM5fhYyeg4AAAD39/fW6Oj/7E/+1jn1uiebvb3xnSVVqy1Smi4XfASspZShAAAAEHRSTlMAAAAAAAAAAAAAAAAAAAAAHik/2wAAAGZJREFUeNptyFEOAiAMBNGqiCJWdgWE3v+ixsSkjXG+Jk/aT/IX5D7BkzgcSIDNgRi9czoA/TkAh8nRX6BDIkBmB6nETOJQz7dtSR308lhaItheSQO0YqvYNYDWZbkFyLojfLLjd97lCA7ioBDbWwAAAABJRU5ErkJggg=='},
+}
+
+export const blocks_palette = {
+    air: {id: 0, faces: [], passable: 1},
+    dirt: {id: 1, faces: [
+        {color: [0.4039, 0.2667, 0.1294], flag: 32, texture: textures.dirt}, // Верх — зеленый
+        {color: [0.4039, 0.2667, 0.1294], flag: 32, texture: textures.dirt}, // Низ — коричневый
+        {color: [0.4039, 0.2667, 0.1294], flag: 32, texture: textures.dirt}, // Перед — коричневый
+        {color: [0.4039, 0.2667, 0.1294], flag: 32, texture: textures.dirt}, // Зад — коричневый
+        {color: [0.4039, 0.2667, 0.1294], flag: 32, texture: textures.dirt}, // Лево — коричневый
+        {color: [0.4039, 0.2667, 0.1294], flag: 32, texture: textures.dirt}, // Право — коричневый
+    ], is_solid: true},
+    grass_block: {id: 2, faces: [
+        {color: [99/256, 127/256, 64/256], texture: textures.grass_block_top}, // Верх — зеленый
+        {color: [0.4039, 0.2667, 0.1294], texture: textures.dirt}, // Низ — коричневый
+        {color: [0.4039, 0.2667, 0.1294], flag: 2 | 32, texture: textures.dirt}, // Перед — коричневый
+        {color: [0.4039, 0.2667, 0.1294], flag: 2 | 32, texture: textures.dirt}, // Зад — коричневый
+        {color: [0.4039, 0.2667, 0.1294], flag: 2 | 32, texture: textures.dirt}, // Лево — коричневый
+        {color: [0.4039, 0.2667, 0.1294], flag: 2 | 32, texture: textures.dirt}, // Право — коричневый
+    ], is_solid: true},
+    stone: {id: 3, faces: [
+        {color: [0.5451, 0.5333, 0.5294], texture: textures.stone}, // Верх — светло-серый
+        {color: [0.5451, 0.5333, 0.5294], texture: textures.stone}, // Низ — темно-серый
+        {color: [0.5451, 0.5333, 0.5294], texture: textures.stone}, // Перед — темно-серый (с оттенком)
+        {color: [0.5451, 0.5333, 0.5294], texture: textures.stone}, // Зад — темно-серый (с оттенком)
+        {color: [0.5451, 0.5333, 0.5294], texture: textures.stone}, // Лево — темно-серый
+        {color: [0.5451, 0.5333, 0.5294], texture: textures.stone}, // Право — темно-серый
+    ], is_solid: true},
+    water: {id: 4, faces: [
+        {color: [63/256, 118/256, 228/256], flag: 1, texture: textures.water_still}, // Верх — синий
+        null, // {color: [63/256, 118/256, 228/256], texture: textures.water_still}, // Низ — синий
+        null, // {color: [63/256, 118/256, 228/256], texture: textures.water_still}, // Перед — синий
+        null, // {color: [63/256, 118/256, 228/256], texture: textures.water_still}, // Зад — синий
+        null, // {color: [63/256, 118/256, 228/256], texture: textures.water_still}, // Лево — синий
+        null, // {color: [63/256, 118/256, 228/256], texture: textures.water_still}, // Право — синий
+    ], is_solid: true, transparent: true, passable: 1},
+    sand: {id: 5, faces: [
+        {color: [0.8, 0.8, 0.6], texture: textures.sand}, // Верх — светло-коричневый
+        {color: [0.8, 0.8, 0.6], texture: textures.sand}, // Низ — светло-коричневый
+        {color: [0.8, 0.8, 0.6], texture: textures.sand}, // Перед — светло-коричневый
+        {color: [0.8, 0.8, 0.6], texture: textures.sand}, // Зад — светло-коричневый
+        {color: [0.8, 0.8, 0.6], texture: textures.sand}, // Лево — светло-коричневый
+        {color: [0.8, 0.8, 0.6], texture: textures.sand}, // Право — светло-коричневый
+    ], is_solid: true},
+    oak_log: {id: 6, faces: [
+        {color: [0.7, 0.7, 0.5], texture: textures.oak_log_top}, // Верх — коричневый
+        {color: [0.7, 0.7, 0.5], texture: textures.oak_log_top}, // Низ — коричневый
+        {color: [146/256, 108/256, 77/256], flag: 8, texture: textures.oak_log}, // Перед — коричневый
+        {color: [146/256, 108/256, 77/256], flag: 8, texture: textures.oak_log}, // Зад — коричневый
+        {color: [146/256, 108/256, 77/256], flag: 8, texture: textures.oak_log}, // Лево — коричневый
+        {color: [146/256, 108/256, 77/256], flag: 8, texture: textures.oak_log}, // Право — коричневый
+    ], is_solid: true},
+    oak_leaves: {id: 7, faces: [
+        {color: [0.0, 0.6, 0.0], flag: 16, texture: textures.oak_leaves}, // Верх — зеленый
+        {color: [0.0, 0.6, 0.0], flag: 16, texture: textures.oak_leaves}, // Низ — зеленый
+        {color: [0.0, 0.6, 0.0], flag: 16, texture: textures.oak_leaves}, // Перед — зеленый
+        {color: [0.0, 0.6, 0.0], flag: 16, texture: textures.oak_leaves}, // Зад — зеленый
+        {color: [0.0, 0.6, 0.0], flag: 16, texture: textures.oak_leaves}, // Лево — зеленый
+        {color: [0.0, 0.6, 0.0], flag: 16, texture: textures.oak_leaves}, // Право — зеленый
+    ], receive_ao: true},
+    bedrock: {id: 8, faces: [
+        {color: [0.1, 0.1, 0.1], texture: textures.bedrock}, // Верх — черный
+        {color: [0.1, 0.1, 0.1], texture: textures.bedrock}, // Низ — черный
+        {color: [0.1, 0.1, 0.1], texture: textures.bedrock}, // Перед — черный
+        {color: [0.1, 0.1, 0.1], texture: textures.bedrock}, // Зад — черный
+        {color: [0.1, 0.1, 0.1], texture: textures.bedrock}, // Лево — черный
+        {color: [0.1, 0.1, 0.1], texture: textures.bedrock}, // Право — черный
+    ], is_solid: true},
+    gravel: {id: 9, faces: [
+        {color: [0.5, 0.5, 0.5], flag: 4, texture: textures.gravel}, // Верх — серый
+        {color: [0.5, 0.5, 0.5], flag: 4, texture: textures.gravel}, // Низ — серый
+        {color: [0.5, 0.5, 0.5], flag: 4, texture: textures.gravel}, // Перед — серый
+        {color: [0.5, 0.5, 0.5], flag: 4, texture: textures.gravel}, // Зад — серый
+        {color: [0.5, 0.5, 0.5], flag: 4, texture: textures.gravel}, // Лево — серый
+        {color: [0.5, 0.5, 0.5], flag: 4, texture: textures.gravel}, // Право — серый
+    ], is_solid: true},
+    short_grass: {id: 10, faces: [
+        {color: [1, 1, 1], flag: 128 | 64, texture: textures.short_grass},
+    ], style: 'planting', passable: 1},
+    dandelion: {id: 11, faces: [
+        {color: [1, 1, 1], flag: 128, texture: textures.dandelion},
+    ], style: 'planting', passable: 1},
+    oxeye_daisy: {id: 12, faces: [
+        {color: [1, 1, 1], flag: 128, texture: textures.oxeye_daisy},
+    ], style: 'planting', passable: 1},
+}
+
+export const grasses = [blocks_palette.short_grass]
+export const plants = [
+    blocks_palette.dandelion,
+    blocks_palette.oxeye_daisy,
+    // poppy, blue_orchid, allium, azure_bluet, red_tulip, orange_tulip, white_tulip, pink_tulip, cornflower, lily_of_the_valley, sweet_berry_bush
+]
---
+++ worker.js
@@ -0,0 +1,197 @@
+let SimplexNoise = null
+let noise2d
+let blocks
+const seed = 1
+const water_level = 64
+const tree_chance = 64
+
+// Очередь для сообщений
+const messageQueue = []
+
+// Флаг для проверки, все ли модули загружены
+let importsLoaded = false
+
+// Функция для обработки очереди сообщений
+function processQueue() {
+    while (messageQueue.length > 0 && importsLoaded) {
+        const event = messageQueue.shift()
+        handleMessage(event)
+    }
+}
+
+// Импорты
+Promise.all([
+    import('../../vendors/simplex-noise.js').then(async (module) => {
+        SimplexNoise = module
+        noise2d = module.createNoise2D(pseudoRandom(seed))
+    }),
+    import('./blocks.js').then(async (module) => {
+        blocks = module
+    })
+]).then(() => {
+    importsLoaded = true
+    processQueue() // Начинаем обработку очереди сообщений
+})
+
+// Обработчик сообщений
+function handleMessage(event) {
+    const { cmd, data } = event.data
+
+    // console.log('Worker received command:', SimplexNoise, noise2d)
+
+    switch (cmd) {
+        case 'generateChunk': {
+            const chunk = new Chunk(data.addr, data.coord, data.size)
+            const blocks = chunk.generate()
+            postMessage({ cmd: 'chunkBlocks', data: { ...data, blocks } })
+            break
+        }
+    }
+}
+
+// Получение сообщений
+onmessage = (event) => {
+    if (importsLoaded) {
+        handleMessage(event)
+    } else {
+        messageQueue.push(event) // Добавляем сообщение в очередь
+    }
+}
+
+
+function pseudoRandom(seed) {
+    let value = (seed < 0 ? seed + 2147483647 : seed) % 2147483647
+    return function() {
+        value = (value * 16807) % 2147483647
+        return value / 2147483647
+    }
+}
+
+class Chunk {
+
+    constructor(addr, coord, size) {
+        this.addr = addr
+        this.coord = coord
+        this.size = size
+        this.blocks = new Uint8Array(size.x * size.y * size.z)
+    }
+
+    setBlock(x, y, z, block) {
+        x = Math.floor(x)
+        y = Math.floor(y)
+        z = Math.floor(z)
+        if(!this.isInBounds(x, y, z)) return
+        const index = this.getIndex(x, y, z)
+        if(!block?.id) debugger
+        this.blocks[index] = block.id
+    }
+
+    getIndex(x, y, z) {
+        return x + y * this.size.x + z * this.size.x * this.size.y
+    }
+
+    isInBounds(x, y, z) {
+        return x >= 0 && x < this.size.x &&
+            y >= 0 && y < this.size.y &&
+            z >= 0 && z < this.size.z
+    }
+
+    // Генератор
+    generate() {
+
+        const { size, addr, coord } = this
+        const { blocks_palette, grasses, plants } = blocks
+
+        let n0 = 0
+        let n2 = 0
+        let ocean = 0
+        let planing = 0
+
+        const setBlock = this.setBlock.bind(this)
+
+        function getXZNoise(x, z) {
+            const x2 = x + 1024
+            const z2 = z + 1024
+            n0 = noise2d(x / 2, z / 2)
+            const n1 = noise2d(x / 64, z / 64)
+            n2 = noise2d(x2 / 16, z2 / 16)
+            const n3 = noise2d(x2 / 8, z2 / 8)
+            return (n1 * 0.7 + n2 * 0.25 + n3 * 0.05)
+        }
+
+        function calcH(x, z) {
+            ocean = noise2d((x - 8192) / 384, (z - 8192) / 384)
+            planing = (noise2d((x - 1024) / 256, (z - 1024) / 256))
+            const height = getXZNoise(x, z) * (32 * ((planing + 1) / 2))
+            return Math.ceil (
+                (64 + ocean * 16) + (height < 0 ? height : height * 2)
+            )
+        }
+
+        function drawTree(x, y, z, random) {
+            const height = Math.ceil(random() * 8 + 2)
+            for(let yy = 0; yy < height; yy++) {
+                setBlock(x, y + yy, z, blocks_palette.oak_log)
+            }
+            // draw leaves
+            const radius = {xz: 3, y: 7}
+            const ax = x
+            const ay = y + height - 2
+            const az = z
+            for(let xx = -radius.xz; xx <= radius.xz; xx++) {
+                for(let yy = 0; yy <= radius.y; yy++) {
+                    for(let zz = -radius.xz; zz <= radius.xz; zz++) {
+                        if(xx * xx + yy * yy + zz * zz <= radius.xz * radius.xz) {
+                            if(xx == 0 && yy < 2 && zz == 0) continue
+                            setBlock(ax + xx, ay + yy, az + zz, blocks_palette.oak_leaves)
+                        }
+                    }
+                }
+            }
+        }
+
+        const random = pseudoRandom(addr.x * 256 + addr.z)
+
+        for(let x = 0; x < size.x; x++) {
+            for(let z = 0; z < size.z; z++) {
+                const h = calcH(x + coord.x, z + coord.z)
+                const max_y = Math.max(h, water_level)
+                for(let y = 0; y < max_y; y++) {
+                    if(y < h) {
+                        let block = blocks_palette.stone
+                        const y_from_top = h - y
+                        if(y < 80 + n2 * (8 * planing)) {
+                            if(y == water_level - 1 && y == h - 1) {
+                                block = blocks_palette.sand
+                            } else if(y < n0 * 2 + 3) {
+                                block = blocks_palette.bedrock
+                            } else {
+                                if(y_from_top == 1 && y > water_level - 1) {
+                                    block = blocks_palette.grass_block
+                                    let gr = random()
+                                    if(gr < .1) {
+                                        gr /= .1
+                                        const block = gr < .9 ? grasses[Math.floor((gr/.9) * grasses.length)] : plants[Math.floor(((gr-.9)/.1) * plants.length)]
+                                        setBlock(x, y + 1, z, block)
+                                    }
+                                } else if(y_from_top < 3) {
+                                    block = y < water_level ? blocks_palette.gravel : blocks_palette.dirt
+                                }
+                            }
+                        }
+                        setBlock(x, y, z, block)
+                    } else { //if(y < water_level && y >= h) {
+                        setBlock(x, y, z, blocks_palette.water)
+                    }
+                }
+                if(h > water_level && h < 75 && Math.round(random() * tree_chance) == 0) {
+                    drawTree(x, h, z, random)
+                }
+            }
+        }
+
+        return this.blocks
+
+    }
+
+}
